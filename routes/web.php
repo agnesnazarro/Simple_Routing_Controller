bbda7929e641bcc018c89154ab1c86bd03f8a2d4
Agnes Nazarro <agnesnazarro@gmail.com>
2024-06-18 22:54:43 +0800

    Web.php: Add comment per route and add route for welcome page

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,14 @@ use App\Http\Controllers\AboutMeController;
 use App\Http\Controllers\SkillsController;
 use App\Http\Controllers\HobbiesController;
 
+//Welcome Page
+Route::get('/', [AboutMeController::class, 'index']);
+
+//About Me Route
 Route::get('/about_me', [AboutMeController::class, 'index']);
 
+//Skills Route
 Route::get('/skills', [SkillsController::class, 'index']);
 
+//Hobbies Route
 Route::get('/hobbies', [HobbiesController::class, 'index']);
