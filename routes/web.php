96ec799f16d43ca6d6a4f9df2052f2c06b02bbef
Agnes Nazarro <agnesnazarro@gmail.com>
2024-06-18 21:40:46 +0800

    Web.php: Connect the routes to their respective controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutMeController;
+use App\Http\Controllers\SkillsController;
+use App\Http\Controllers\HobbiesController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/about_me', [AboutMeController::class, 'index']);
+
+Route::get('/skills', [SkillsController::class, 'index']);
+
+Route::get('/hobbies', [HobbiesController::class, 'index']);
